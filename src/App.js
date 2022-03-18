33421cd732411496bb74ef5551bc0e5e3829c95a
danbahrami <dan.bahrami@googlemail.com>
2022-03-18 18:40:30 +0000

    Add another option

--- src/App.js
+++ src/App.js
@@ -2,28 +2,18 @@ import { useState, useRef, useCallback } from "react";
 import { useDebounce } from "rooks";
 import SidePanel from "./SidePanel";
 import Dropdown from "./Dropdown";
+import { COLOURS, LETTERS } from "./constants";
 import "./App.css";
 
-const COLOURS = [
-  "red",
-  "green",
-  "blue",
-  "orange",
-  "purple",
-  "silver",
-  "black",
-  "white",
-  "maroon",
-  "violet",
-  "indigo",
-];
-
 function App() {
-  const [colour, setColour] = useState("green");
-  const dropdownRef = useRef(null);
+  const [colour, setColour] = useState("chocolate");
+  const [letter, setLetter] = useState(LETTERS[0]);
+  const colourDropdownRef = useRef(null);
+  const letterDropdownRef = useRef(null);
 
   const handleScroll = useCallback(() => {
-    dropdownRef.current.close();
+    colourDropdownRef.current.close();
+    letterDropdownRef.current.close();
   }, []);
 
   const debouncedHandleScroll = useDebounce(handleScroll, 500, {
@@ -36,14 +26,29 @@ function App() {
       <SidePanel onScroll={debouncedHandleScroll}>
         <label htmlFor="colour">Pick a colour</label>
         <Dropdown
-          ref={dropdownRef}
+          ref={colourDropdownRef}
           options={COLOURS}
           value={colour}
           id="colour"
           name="colour"
           onChange={setColour}
         />
+
+        <label htmlFor="letter">Pick a letter</label>
+        <Dropdown
+          ref={letterDropdownRef}
+          options={LETTERS}
+          value={letter}
+          id="letter"
+          name="letter"
+          onChange={setLetter}
+        />
       </SidePanel>
+      <div className="App__result">
+        <div className="App__result__letter" style={{ color: colour }}>
+          {letter}
+        </div>
+      </div>
     </div>
   );
 }
